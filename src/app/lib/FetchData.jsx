74962731c76e5433d1b23ccf466804779e363ecc
Mahedi <mahidehasansiyam123@gmail.com>
2026-05-19 20:38:34 +0600

    ...

--- src/app/lib/FetchData.jsx
+++ src/app/lib/FetchData.jsx
@@ -1,12 +1,12 @@
 export const getfewcardata = async () => {
-  const res = await fetch(`${process.env.HOST_URL}/fewdata`, {
+  const res = await fetch(`${process.env.SERVER_URL}/fewdata`, {
     cache: 'no-store',
   });
   const data = await res.json();
   return data;
 }
 export const getallcardata = async () => {
-  const res = await fetch(`${process.env.HOST_URL}/alldata`, {
+  const res = await fetch(`${process.env.SERVER_URL}/alldata`, {
     cache: 'no-store',
   });
   const data = await res.json();
